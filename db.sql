b7b1a8199ae6988ab4bb26e395222efe9849f273
Dhanu K Parvateesam <bewithdhanu@gmail.com>
2017-06-19 15:46:35 +0530

    Update db.sql
    Data added to db.sql file by admin

--- db.sql
+++ db.sql
@@ -33,7 +33,13 @@ CREATE TABLE IF NOT EXISTS `tbl_items` (
   PRIMARY KEY (`itemId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table cias.tbl_items: 2 rows
+/*!40000 ALTER TABLE `tbl_items` DISABLE KEYS */;
+REPLACE INTO `tbl_items` (`itemId`, `itemHeader`, `itemSub`, `itemDesc`, `itemImage`, `isDeleted`, `createdBy`, `createdDtm`, `updatedDtm`, `updatedBy`) VALUES
+	(1, 'jquery.validation.js', 'Contribution towards jquery.validation.js', 'jquery.validation.js is the client side javascript validation library authored by Jörn Zaefferer hosted on github for us and we are trying to contribute to it. Working on localization now', 'validation.png', 0, 1, '2015-09-02 00:00:00', NULL, NULL),
+	(2, 'CodeIgniter User Management', 'Demo for user management system', 'This the demo of User Management System (Admin Panel) using CodeIgniter PHP MVC Framework and AdminLTE bootstrap theme. You can download the code from the repository or forked it to contribute. Usage and installation instructions are provided in ReadMe.MD', 'cias.png', 0, 1, '2015-09-02 00:00:00', NULL, NULL);
+/*!40000 ALTER TABLE `tbl_items` ENABLE KEYS */;
+
 -- Dumping structure for table cias.tbl_reset_password
 DROP TABLE IF EXISTS `tbl_reset_password`;
 CREATE TABLE IF NOT EXISTS `tbl_reset_password` (
@@ -50,7 +56,10 @@ CREATE TABLE IF NOT EXISTS `tbl_reset_password` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table cias.tbl_reset_password: ~0 rows (approximately)
+/*!40000 ALTER TABLE `tbl_reset_password` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_reset_password` ENABLE KEYS */;
+
 -- Dumping structure for table cias.tbl_roles
 DROP TABLE IF EXISTS `tbl_roles`;
 CREATE TABLE IF NOT EXISTS `tbl_roles` (
@@ -59,7 +68,14 @@ CREATE TABLE IF NOT EXISTS `tbl_roles` (
   PRIMARY KEY (`roleId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table cias.tbl_roles: 3 rows
+/*!40000 ALTER TABLE `tbl_roles` DISABLE KEYS */;
+REPLACE INTO `tbl_roles` (`roleId`, `role`) VALUES
+	(1, 'System Administrator'),
+	(2, 'Manager'),
+	(3, 'Employee');
+/*!40000 ALTER TABLE `tbl_roles` ENABLE KEYS */;
+
 -- Dumping structure for table cias.tbl_users
 DROP TABLE IF EXISTS `tbl_users`;
 CREATE TABLE IF NOT EXISTS `tbl_users` (
@@ -77,7 +93,14 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
   PRIMARY KEY (`userId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table cias.tbl_users: 3 rows
+/*!40000 ALTER TABLE `tbl_users` DISABLE KEYS */;
+REPLACE INTO `tbl_users` (`userId`, `email`, `password`, `name`, `mobile`, `roleId`, `isDeleted`, `createdBy`, `createdDtm`, `updatedBy`, `updatedDtm`) VALUES
+	(1, 'admin@bewithdhanu.in', '$2y$10$SAvFim22ptA9gHVORtIaru1dn9rhgerJlJCPxRNA02MjQaJnkxawq', 'System Administrator', '9890098900', 1, 0, 0, '2015-07-01 18:56:49', 1, '2017-06-19 09:22:53'),
+	(2, 'manager@bewithdhanu.in', '$2y$10$Gkl9ILEdGNoTIV9w/xpf3.mSKs0LB1jkvvPKK7K0PSYDsQY7GE9JK', 'Manager', '9890098900', 2, 0, 1, '2016-12-09 17:49:56', 1, '2017-06-19 09:22:29'),
+	(3, 'employee@bewithdhanu.in', '$2y$10$MB5NIu8i28XtMCnuExyFB.Ao1OXSteNpCiZSiaMSRPQx1F1WLRId2', 'Employee', '9890098900', 3, 0, 1, '2016-12-09 17:50:22', 1, '2017-06-19 09:23:21');
+/*!40000 ALTER TABLE `tbl_users` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
